5cc5a95b0a5b6cd4eac721c416a21c621bc05054
Kento <hide.zilch@gmail.com>
2014-06-29 17:19:30 +0900

    Under implement task3
    have to implement image view of selected item.

--- src/uchida/eurekatask1/MainActivity.java
+++ src/uchida/eurekatask1/MainActivity.java
@@ -1,14 +1,6 @@
 
 package uchida.eurekatask1;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import uchida.eurekatask1.MyFragment.Dribbble;
 import android.app.ActionBar;
 import android.app.ActionBar.Tab;
 import android.app.ActionBar.TabListener;
@@ -19,91 +11,30 @@ import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.util.Log;
-
-import com.android.volley.Request.Method;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.Response.Listener;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.ImageLoader;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends FragmentActivity implements TabListener {
 
     ViewPager mViewPager;
-    private RequestQueue mQueue;
-    private ImageLoader mImageLoader;
-    private String mNextPageToken;
-    private List<Dribbble> dribbbleList;
-    private List<List<Dribbble>> list;
 
-    final String[] tabTitle = {
+    private final String[] TAB_TITLE = {
             "everyone", "debuts", "popular"
     };
 
-    private final String urlEveryone = "http://api.dribbble.com/shots/everyone";
-    private final String urlDebuts = "http://api.dribbble.com/shots/debuts";
-    private final String urlPopular = "http://api.dribbble.com/shots/popular";
+    private final String URL_EVERYONE = "http://api.dribbble.com/shots/everyone";
+    private final String URL_DEBUTS = "http://api.dribbble.com/shots/debuts";
+    private final String URL_POPULAR = "http://api.dribbble.com/shots/popular";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        list = new ArrayList<List<Dribbble>>();
-        Log.d("test", "test onCreate");
-        mQueue = Volley.newRequestQueue(this);
-        mQueue.add(getJsonRequest(urlEveryone));
-        mQueue.add(getJsonRequest(urlDebuts));
-        mQueue.add(getJsonRequest(urlPopular));
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            // TODO 自動生成された catch ブロック
-            e.printStackTrace();
-        }
-        temp();
-    }
-
-    private JsonObjectRequest getJsonRequest(String url) {
-        return new JsonObjectRequest(Method.GET, url, null,
-                new Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        dribbbleList = new ArrayList<Dribbble>();
-                        try {
-                            JSONArray shots = response.getJSONArray("shots");
-
-                            for (int i = 0; i < shots.length(); i++) {
-                                JSONObject shot = shots.getJSONObject(i);
-                                JSONObject player = shot.getJSONObject("player");
-                                dribbbleList.add(new Dribbble(shot.getString("title"),
-                                        shot.getString("image_url"), shot.getString("likes_count"),
-                                        player.getString("name")));
-                            }
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-                        list.add(dribbbleList);
-                        Log.w("test DOYA!", "test size=" + dribbbleList.size());
-                    }
-                },
-
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                    }
-                });
-    }
-
-    public void temp() {
+        // ActionBarにタブを設定
         ActionBar actionBar = getActionBar();
         actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
 
+        // PagerにFragmentを設定
         MyPagerAdapter adapter = new MyPagerAdapter(getSupportFragmentManager());
-
         mViewPager = (ViewPager) findViewById(R.id.pager);
         mViewPager.setAdapter(adapter);
         mViewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
@@ -114,6 +45,7 @@ public class MainActivity extends FragmentActivity implements TabListener {
             }
         });
 
+        // タブ項目追加
         for (int i = 0; i < adapter.getCount(); i++) {
             actionBar.addTab(actionBar.newTab()
                     .setText(adapter.getPageTitle(i))
@@ -130,19 +62,29 @@ public class MainActivity extends FragmentActivity implements TabListener {
         public Fragment getItem(int position) {
             MyFragment fragment = new MyFragment();
             Bundle data = new Bundle();
-            Log.d("test", "test getItem");
-
-            return new MyFragment(list.get(position));
+            switch (position) {
+                case 0:
+                    data.putString(getString(R.string.KEY_URL), URL_EVERYONE);
+                    break;
+                case 1:
+                    data.putString(getString(R.string.KEY_URL), URL_DEBUTS);
+                    break;
+                case 2:
+                    data.putString(getString(R.string.KEY_URL), URL_POPULAR);
+                    break;
+            }
+            fragment.setArguments(data);
+            return fragment;
         }
 
         @Override
         public int getCount() {
-            return tabTitle.length;
+            return TAB_TITLE.length;
         }
 
         @Override
         public CharSequence getPageTitle(int position) {
-            return tabTitle[position];
+            return TAB_TITLE[position];
         }
     }
 
--- src/uchida/eurekatask1/MyFragment.java
+++ src/uchida/eurekatask1/MyFragment.java
@@ -1,39 +1,52 @@
 
 package uchida.eurekatask1;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.app.Dialog;
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
-import android.util.Log;
+import android.support.v4.util.LruCache;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.android.volley.Request.Method;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.Response.Listener;
+import com.android.volley.VolleyError;
 import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.ImageLoader.ImageCache;
 import com.android.volley.toolbox.ImageLoader.ImageContainer;
+import com.android.volley.toolbox.ImageLoader.ImageListener;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 
 public class MyFragment extends ListFragment {
 
+    final String TAG = "MyFragment";
     private List<Dribbble> mDribbbleList;
-
-    MyFragment(List<Dribbble> dribbbleList) {
-        this.mDribbbleList = dribbbleList;
-        Log.d("test DA!", "test size=" + dribbbleList.size());
-    }
-
-    public MyFragment() {
-        // TODO 自動生成されたコンストラクター・スタブ
-    }
+    private RequestQueue mQueue;
+    private String mUrl;
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+
+        mDribbbleList = new ArrayList<Dribbble>();
 
         // ListViewの形成
         ListView listView = getListView();
@@ -41,29 +54,62 @@ public class MyFragment extends ListFragment {
         listView.setVerticalScrollBarEnabled(false);
         listView.setSelector(android.R.color.transparent);
 
-        // Adapterを生成してセット
-        setListAdapter(new CustomAdapter(getActivity(), mDribbbleList));
+        // URLの取得
+        mUrl = getArguments().getString(getString(R.string.KEY_URL));
+
+        // リクエストの発行
+        mQueue = Volley.newRequestQueue(getActivity());
+        mQueue.add(getJsonRequest(mUrl));
+    }
+
+    private JsonObjectRequest getJsonRequest(String url) {
+        return new JsonObjectRequest(Method.GET, url, null,
+                new Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONArray shots = response.getJSONArray("shots");
+
+                            for (int i = 0; i < shots.length(); i++) {
+                                JSONObject shot = shots.getJSONObject(i);
+                                JSONObject player = shot.getJSONObject("player");
+                                mDribbbleList.add(new Dribbble(shot.getString("title"),
+                                        shot.getString("image_url"), shot.getString("likes_count"),
+                                        player.getString("name")));
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                        // Adapterを生成してセット
+                        setListAdapter(new CustomAdapter(getActivity(), mDribbbleList));
+                    }
+                },
+
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                    }
+                });
     }
 
     @Override
     public void onListItemClick(ListView l, View v, int position, long id) {
         super.onListItemClick(l, v, position, id);
 
-        // // 選択された１行を取得
-        // Dribbble dribbble = (Dribbble) l.getItemAtPosition(position);
-        //
-        // // イメージ取得
-        // ImageView iv = new ImageView(getActivity());
-        // iv.setImageBitmap(BitmapFactory.decodeResource(getResources(),
-        // dribbble.getImage()));
-        // iv.setScaleType(ImageView.ScaleType.FIT_XY);
-        // iv.setAdjustViewBounds(true);
-        //
-        // // ダイアログ表示
-        // Dialog dialog = new Dialog(getActivity());
-        // dialog.getWindow().requestFeature(Window.FEATURE_NO_TITLE);
-        // dialog.setContentView(iv);
-        // dialog.show();
+        // 選択された１行を取得
+        Dribbble dribbble = (Dribbble) l.getItemAtPosition(position);
+
+        // イメージ取得
+        ImageView iv = new ImageView(getActivity());
+        iv.setScaleType(ImageView.ScaleType.FIT_XY);
+        iv.setAdjustViewBounds(true);
+
+        // ダイアログ表示
+        Dialog dialog = new Dialog(getActivity());
+        dialog.getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(iv);
+        dialog.show();
     }
 
     public static class Dribbble {
@@ -104,6 +150,30 @@ public class MyFragment extends ListFragment {
         TextView likes;
     }
 
+    public class BitmapCache implements ImageCache {
+        private LruCache<String, Bitmap> mCache;
+
+        public BitmapCache() {
+            int maxSize = 10 * 1024 * 1024;
+            mCache = new LruCache<String, Bitmap>(maxSize) {
+                @Override
+                protected int sizeOf(String key, Bitmap value) {
+                    return value.getRowBytes() * value.getHeight();
+                }
+            };
+        }
+
+        @Override
+        public Bitmap getBitmap(String url) {
+            return mCache.get(url);
+        }
+
+        @Override
+        public void putBitmap(String url, Bitmap bitmap) {
+            mCache.put(url, bitmap);
+        }
+    }
+
     public class CustomAdapter extends ArrayAdapter<Dribbble> {
         LayoutInflater mInflater;
         private ImageLoader mImageLoader;
@@ -111,7 +181,7 @@ public class MyFragment extends ListFragment {
         public CustomAdapter(Context context, List<Dribbble> objects) {
             super(context, 0, objects);
             mInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            mImageLoader = new ImageLoader(null, null);
+            mImageLoader = new ImageLoader(mQueue, new BitmapCache());
         }
 
         @Override
@@ -132,29 +202,27 @@ public class MyFragment extends ListFragment {
                 holder = (ViewHolder) convertView.getTag();
             }
 
-            // 写真とテキストをセットする
-            final Dribbble dribbble = (Dribbble) this.getItem(position);
+            Dribbble dribbble = (Dribbble) this.getItem(position);
             if (dribbble != null) {
+                // テキストをセット
                 holder.title.setText(dribbble.getTitle());
                 holder.name.setText(dribbble.getPlayer());
                 holder.likes.setText("Like: " + dribbble.getLikes());
 
-                ImageContainer imageContainer = (ImageContainer) holder.image.getTag();
+                // 画像を取得
+                ImageContainer imageContainer = (ImageContainer)
+                        holder.image.getTag();
                 if (imageContainer != null) {
                     imageContainer.cancelRequest();
                 }
 
-                Log.d("test", "test image =" + dribbble.getImage());
-                // ImageListener listener =
-                // ImageLoader.getImageListener(holder.image,
-                // android.R.drawable.spinner_background /* 表示待ち時の画像 */,
-                // android.R.drawable.ic_dialog_alert /* エラー時の画像 */);
-                //
-                // Log.d("test", "test koko");
-                // holder.image.setTag(mImageLoader.get(dribbble.getImage(),
-                // listener));
+                ImageListener listener =
+                        ImageLoader.getImageListener(holder.image,
+                                android.R.drawable.spinner_background,
+                                android.R.drawable.ic_dialog_alert);
 
-                Log.d("test", "test saigo");
+                // 画像をセット
+                holder.image.setTag(mImageLoader.get(dribbble.getImage(), listener));
             }
 
             return convertView;
